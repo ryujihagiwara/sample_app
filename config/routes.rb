f6ce14ffa9632ac96d5febfb2941cde65581dcbd
ryujihagiwara <dragon7103next@icloud.com>
2020-07-20 22:40:28 +0900

    [Start]3-9 削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
