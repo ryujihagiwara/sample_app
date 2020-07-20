9d995876890d0b8a589346326df0bd796cb6d388
ryujihagiwara <dragon7103next@icloud.com>
2020-07-20 16:29:44 +0900

    [Finish]3-7 編集機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,7 +4,6 @@ class TodolistsController < ApplicationController
   end
 
   def create
-
   	list=List.new(list_params)
 
   	list.save
@@ -22,6 +21,15 @@ class TodolistsController < ApplicationController
 
   def edit
     @list=List.find(params[:id])
+  end
+
+  def update
+    list=List.find(params[:id])
+
+    list.update(list_params)
+
+    redirect_to todolist_path(list.id)
+
   end
 
   private
