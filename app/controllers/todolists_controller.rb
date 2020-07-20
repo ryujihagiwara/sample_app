9a2fb9f0a63461bf037efd30382142f5c2453d2f
ryujihagiwara <dragon7103next@icloud.com>
2020-07-20 22:54:10 +0900

    [Finish]3-9 削除機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -32,6 +32,15 @@ class TodolistsController < ApplicationController
 
   end
 
+  def destroy
+    list=List.find(params[:id])
+
+    list.destroy
+
+    redirect_to todolists_path
+
+  end
+
   private
 
   def list_params
