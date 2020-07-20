6421cd721beb4e4051eb491631a38dc4f998bbda
ryujihagiwara <dragon7103next@icloud.com>
2020-07-20 16:00:37 +0900

    [Start]3-7 編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
